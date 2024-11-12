5a57800af259485c1e95e236d58c099331dd3c69
gaby <gabrielmuise765@gmail.com>
2024-11-12 20:01:21 -0300

    Style del form

--- View/FormInicio.Designer.cs
+++ View/FormInicio.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.pictureBoxImageInicio = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImageInicio)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.BackColor = System.Drawing.Color.White;
+            this.btnEntrar.FlatAppearance.BorderSize = 0;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.Color.Blue;
+            this.btnEntrar.Location = new System.Drawing.Point(212, 326);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(358, 56);
+            this.btnEntrar.TabIndex = 1;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxImageInicio
+            // 
+            this.pictureBoxImageInicio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxImageInicio.Image = global::SistemaAlquileres.Properties.Resources.logoInicio;
+            this.pictureBoxImageInicio.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxImageInicio.Name = "pictureBoxImageInicio";
+            this.pictureBoxImageInicio.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxImageInicio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxImageInicio.TabIndex = 0;
+            this.pictureBoxImageInicio.TabStop = false;
+            // 
+            // FormInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.pictureBoxImageInicio);
+            this.Name = "FormInicio";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImageInicio)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.PictureBox pictureBoxImageInicio;
     }
 }
 
